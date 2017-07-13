c05e34cabc4469877bff9790dbd860c6b909f845
matrixation <25352820@qq.com>
2017-07-13 09:05:41 +0800

    commit

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -22,16 +22,23 @@
       </li>
     </ul>
   </div>
-  <div class="list-fixed" v-show="fixedTitle">
+  <div class="list-fixed" v-show="fixedTitle" ref="fixed">
     <h1 class="fixed-title">{{fixedTitle}}</h1>
+  </div>
+  <div v-show="!data.length" class="loading-container">
+    <loading></loading>
   </div>
 </scroll>
 </template>
 
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
-import {getData} from 'common/js/dom'
-let ANCHOR_HEIGHT = 18
+import Loading from 'base/loading/loading'
+import {
+  getData
+} from 'common/js/dom'
+const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 export default {
   created() {
     this.touch = {}
@@ -42,7 +49,8 @@ export default {
   data() {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1
     }
   },
   props: {
@@ -58,7 +66,7 @@ export default {
       })
     },
     fixedTitle() {
-      if(this.scrollY > 0){
+      if (this.scrollY > 0) {
         return ''
       }
       return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
@@ -86,12 +94,12 @@ export default {
       this.scrollY = pos.y
     },
     _scrollTo(index) {
-      if(!index && index != 0){
+      if (!index && index != 0) {
         return
       }
-      if(index < 0){
+      if (index < 0) {
         index = 0
-      }else if(index > this.listHeight.length -2){
+      } else if (index > this.listHeight.length - 2) {
         index = this.listHeight - 2
       }
       this.scrollY = -this.listHeight[index]
@@ -116,23 +124,33 @@ export default {
     },
     scrollY(newY) {
       const listHeight = this.listHeight
-      if(newY > 0){
+      if (newY > 0) {
         this.currentIndex = 0
         return
       }
-      for (let i = 0; i < listHeight.length -1; i++) {
+      for (let i = 0; i < listHeight.length - 1; i++) {
         let height1 = listHeight[i]
         let height2 = listHeight[i + 1]
         if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return
         }
       }
-      this.currentIndex = listHeight.length -2
+      this.currentIndex = listHeight.length - 2
+    },
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
     }
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   }
 }
 </script>
